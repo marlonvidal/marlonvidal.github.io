8f45ccd02bc669f0048c8124998f3e928ff25210
Marlon VidalFerreira <MVidalFerreira@fanatics.com>
2018-11-29 10:47:03 -0200

    Alterado projeto marlonvidal.com.br

---
+++ inc/sendEmail.php
@@ -0,0 +1,70 @@
+﻿<?php
+
+// Replace this with your own email address
+$siteOwnersEmail = 'contato@marlonvidal.com.br';
+
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['contactName']));
+   $email = trim(stripslashes($_POST['contactEmail']));
+   $subject = trim(stripslashes($_POST['contactSubject']));
+   $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+   // Check Name
+	if (strlen($name) < 2) {
+		$error['name'] = "Digite seu nome.";
+	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Digite um e-mail válido.";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Digite sua mensagem. Ela deve ter no mínimo 15 caracteres";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Contato - Site Marlon Vidal"; }
+
+
+   // Set Message
+   $message .= "Nome: " . $name . "<br />";
+	$message .= "Email: " . $email . "<br />";
+   $message .= "Mensagem: <br />";
+   $message .= $contact_message;
+   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   //$from =  $name . " <" . $email . ">";
+   $from =  $name . " <contato@ditiz.com.br>";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
+
+}
+
+?>
--- js/enums.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var TipoPalavraChave = {
-    Exata: {value: 'Exact'},
-    Ampla: {value: 'Broad'},
-    Frase: {value: 'Phrase'}
-}
